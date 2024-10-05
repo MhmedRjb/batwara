3556f1f411129a6d75bc7a669833041475731ce3
MhmedRjb <mohamed2elfateh@gmail.com>
2024-10-06 00:29:21 +0300

    chore :redirect to login and add dashboard

--- batwara/batwara/doctype/expense/test_expense.py
+++ batwara/batwara/doctype/expense/test_expense.py
@@ -4,28 +4,43 @@
 import frappe
 from frappe.tests.utils import FrappeTestCase
 
+import frappe
+from frappe.tests.utils import FrappeTestCase
+
 class TestExpense(FrappeTestCase):
-	def test_equal_split_calculation(self):
-		#create a test users before
-		test_expense = frappe.get_doc({
-			"doctype": "Expense",
-			"paid_by": "nada@gmail.com",
-			"amount": 100,	
-			"splits": [
-				{
-					"user": "yomna@gmail.com"
-					
-				},
-				{
-					"user": "menna@gmail.com"
-				}
-    
-			],
-			"description": "Test Expense",
-			"split_method": "Equally",
-			"currency": "USD"
-			
-		}).insert()
+    def test_equal_split_calculation(self):
+        # Create test users if they do not exist
+        if not frappe.db.exists("User", "test1@gmail.com"):
+            user1 = frappe.get_doc({
+                "doctype": "User",
+                "email": "test1@gmail.com",
+                "first_name": "test",
+            }).insert()
   
-		self.assertEqual(test_expense.splits[0].amount, 50)
-		self.assertEqual(test_expense.splits[1].amount, 50)
+        if not frappe.db.exists("User", "test2@gmail.com"):
+            user2 = frappe.get_doc({
+                "doctype": "User",
+                "email": "test2@gmail.com",
+                "first_name": "test1",
+            }).insert()
+  
+        # Create test expense
+        test_expense = frappe.get_doc({
+            "doctype": "Expense",
+            "paid_by": "nada@gmail.com",
+            "amount": 100,	
+            "splits": [
+                {
+                    "user": "test1@gmail.com"
+                },
+                {
+                    "user": "test2@gmail.com"
+                }
+            ],
+            "description": "Test Expense",
+            "split_method": "Equally",
+            "currency": "USD"
+        }).insert()
+  
+        self.assertEqual(test_expense.splits[0].amount, 50)
+        self.assertEqual(test_expense.splits[1].amount, 50)
